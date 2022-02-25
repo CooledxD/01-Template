842e2de8f87dd4abd9b868825c075ec3840606e9
CooledxD <scribe_85@mail.ru>
2022-02-26 02:47:29 +0700

    configuring webpack

---
+++ webpack.config.js
@@ -0,0 +1,70 @@
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+
+let mode = 'development'
+if (process.env.NODE_ENV === 'production') {
+  mode = 'production'
+}
+
+module.exports = {
+  mode: mode,
+
+  entry: {
+    main: './src/index.js'
+  },
+
+  devtool: 'source-map',
+
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: '[name].[contenthash].js',
+    assetModuleFilename: 'assets/[hash][ext][query]',
+    clean: true,
+  },
+
+  devServer: {
+    hot: true,
+    open: true,
+    static: {
+      directory: path.resolve(__dirname, './src'),
+      watch: true
+    }
+  },
+
+  optimization: {
+    splitChunks: {
+      chunks: 'all'
+    }
+  },
+
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/pages/index.html'
+    }),
+    new MiniCssExtractPlugin({
+      filename: '[name].[contenthash].css'
+    })
+  ],
+
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: [MiniCssExtractPlugin.loader, 'css-loader']
+      },
+      {
+        test: /\.(png|svg||jpe?g|gif|webp|ico)$/i,
+        type: 'asset/resource'
+      },
+      {
+        test: /\.woff2$/i,
+        type: 'asset/resource'
+      },
+      {
+        test: /\.html$/i,
+        loader: 'html-loader'
+      }
+    ]
+  }
+}
